07c123d8152579380d5536b69b1aa35a94ba8c5b
camilu <camilu.fer21@gmail.com>
2021-06-13 23:35:20 -0400

    firebaseconfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAYq-JsCBXY2w62_WA7RfEimM0CKRMM8-U",
+    authDomain: "rifalo-todo-64fe0.firebaseapp.com",
+    projectId: "rifalo-todo-64fe0",
+    storageBucket: "rifalo-todo-64fe0.appspot.com",
+    messagingSenderId: "1002846091423",
+    appId: "1:1002846091423:web:fe7fd47510e77f00a94aaa",
+    measurementId: "G-SDBF7Y0NZE"
+  }
 };
