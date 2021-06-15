ec25c1cade5856d6a671e83c4216c05c3909eaa4
camilu <camilu.fer21@gmail.com>
2021-06-15 15:21:52 -0400

    about

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -10,18 +10,23 @@ export class CarouselComponent implements OnInit {
 
   imagenes:any[]=[
     { name:'',
-      img:'./assets/img/imgSlider3.png',
+      img:'./assets/img/imgSlider5.jpg',
     }, 
     {
       name:'',
-      img:'./assets/img/imgSlider4.jpg',
+      img:'./assets/img/imgSlider7.jpg',
+      desc:''
+    },
+    {
+      name:'',
+      img:'./assets/img/imgSlider6.jpg',
       desc:''
     }
   ];
 
 
   constructor(private _config:NgbCarouselConfig) { 
-    _config.interval = 6000;
+    _config.interval = 5000;
     _config.pauseOnHover = true;
     _config.showNavigationArrows = false;
 
